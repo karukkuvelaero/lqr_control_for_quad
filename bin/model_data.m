e5cc9c8b604e0a0a6a24c54a5a1beef440a70953
karukkuvelaero <d.karukkuvelraj@gmail.com>
2024-03-11 18:05:39 +0530

    urupudatha EOD 11/03/2024

--- bin/model_data.m
+++ bin/model_data.m
@@ -1,12 +1,12 @@
 g = 9.81; % acceleration due to gravity
-mass_quad = 1.1; % mass of the quadrotor in kilograms
+mass_quad = 1.5; % mass of the quadrotor in kilograms
 
 M_x_quad = 0.0190;% moment of inertia about x axis in kg*m^2
 M_y_quad = 0.0190; % moment of inertia about y axis in kg*m^2
 M_z_quad = 0.0252;  % moment of inertia about z axis in kg*m^2
-n_c = 0.01; % linear relationship between PWM and thrust
-n_t = 0.010757; % linear relationship between PWM and torque
-L = 0.24; % Length of quad arm in metres
+n_c = 2.021382751247327e-04*10^-3; % linear relationship between PWM and thrust
+n_t = 1.500108339272986*10^-3; % linear relationship between PWM and torque
+L = 0.25; % Length of quad arm in metres
 
 x = 0;
 y = 0;
@@ -43,16 +43,16 @@ quad_actuator_B = [0 0 0 0 ;
                    0 0 0 0 ;
                    0 0 0 0 ;
                    1/mass_quad 0 0 0 ;
-                   0 4*L*sin(theta)/M_x_quad 0 0 ;
-                   0 0 4*L*cos(theta)/M_y_quad 0 ;
-                   0 0 0 4/M_z_quad];
+                   0 4*L*sin(theta)*n_t/M_x_quad 0 0 ;
+                   0 0 4*L*cos(theta)*n_t/M_y_quad 0 ;
+                   0 0 0 4*n_c/M_z_quad];
 
 observer_C = [1 0 0 0 0 0 0 0 0 0 0 0 ;
               0 1 0 0 0 0 0 0 0 0 0 0 ;
               0 0 1 0 0 0 0 0 0 0 0 0 ;
-              0 0 0 1 0 0 0 0 0 0 0 0 ;
-              0 0 0 0 1 0 0 0 0 0 0 0 ;
-              0 0 0 0 0 1 0 0 0 0 0 0 ;
+              0 0 0 0 0 0 0 0 0 0 0 0 ;
+              0 0 0 0 0 0 0 0 0 0 0 0 ;
+              0 0 0 0 0 0 0 0 0 0 0 0 ;
               0 0 0 0 0 0 1 0 0 0 0 0 ;
               0 0 0 0 0 0 0 1 0 0 0 0 ;
               0 0 0 0 0 0 0 0 1 0 0 0 ;
@@ -63,23 +63,29 @@ observer_C = [1 0 0 0 0 0 0 0 0 0 0 0 ;
 matrix_x = [x; y; z; phi; theta; psi; u; v; w; p; q; r];
 D=zeros(12,4);
 
-Q =           [1 0 0 0 0 0 0 0 0 0 0 0 ;
-              0 1 0 0 0 0 0 0 0 0 0 0 ;
-              0 0 1 0 0 0 0 0 0 0 0 0 ;
-              0 0 0 1 0 0 0 0 0 0 0 0 ;
-              0 0 0 0 1 0 0 0 0 0 0 0 ;
-              0 0 0 0 0 1 0 0 0 0 0 0 ;
-              0 0 0 0 0 0 1 0 0 0 0 0 ;
-              0 0 0 0 0 0 0 1 0 0 0 0 ;
-              0 0 0 0 0 0 0 0 10000 0 0 0 ;
-              0 0 0 0 0 0 0 0 0 1000 0 0 ;
-              0 0 0 0 0 0 0 0 0 0 100 0 ;
-              0 0 0 0 0 0 0 0 0 0 0 100 ];
-R = [0.005 0 0 0;
-    0 0.005 0 0;
-    0 0 0.005 0;
-    0 0 0 0.005];
+Q =           [0.001111111 0 0 0 0 0 0 0 0 0 0 0 ;
+              0 0.001111111 0 0 0 0 0 0 0 0 0 0 ;
+              0 0 0.001111111 0 0 0 0 0 0 0 0 0 ;
+              0 0 0 0.001111111 0 0 0 0 0 0 0 0 ;
+              0 0 0 0 0.001111111 0 0 0 0 0 0 0 ;
+              0 0 0 0 0 0.001111111 0 0 0 0 0 0 ;
+              0 0 0 0 0 0 0.001111111 0 0 0 0 0 ;
+              0 0 0 0 0 0 0 0.001111111 0 0 0 0 ;
+              0 0 0 0 0 0 0 0 3 0 0 0 ;
+              0 0 0 0 0 0 0 0 0 5 0 0 ;
+              0 0 0 0 0 0 0 0 0 0 4 0 ;
+              0 0 0 0 0 0 0 0 0 0 0 3];
+R = [1 0 0 0;
+    0 1 0 0;
+    0 0 1 0;
+    0 0 0 1];
 K=lqr(quad_state_A,quad_actuator_B,Q,R)
 n=ctrb(quad_state_A,quad_actuator_B)
 rank(n)
+sys=ss(quad_state_A,quad_actuator_B,observer_C,D)
+step(sys)
+
+K2=lqr(sys,Q,R)
+Y=feedback(sys,K,-1)
+% step(Y)
 
